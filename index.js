4b08f41c1fbc76eb29503ea93eba0f380d214ba0
yego <yegokipkemoi254@gmail.com>
2022-08-24 00:24:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate") {
+ return (`This Saturday, I want to ${activity}!`);
+}
+
+saturdayFun()
+
+ function mondayWork(task = "go to the office") {
+    return `This Monday, I will ${task}.`;
+ }
+  
+ mondayWork()
+
+
+ function wrapAdjective(wrap = "*"){
+   return function(adjective= "a hard worker"){
+       return `You are ${wrap}${adjective}${wrap}!`
+   };
+   };
+   wrapAdjective()
